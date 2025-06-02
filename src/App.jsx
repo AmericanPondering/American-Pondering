5da6dfdc9d2b47be6811710f7da76beecc7fde09
Josiah Tomich <jo.launchpad@gmail.com>
2025-06-02 14:32:27 -0400

    Add copy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import * as copy from "./copy/teaser-page"
+
 function App() {
-  return <></>;
+  return <>
+    <h1>{copy.title}</h1>
+    {/* Cover img */}
+    <h2>{copy.introHeading}</h2>
+    <pre>{copy.introParagraph}</pre>
+
+    <h2>{copy.artistIntroHeading}</h2>
+    <pre>{copy.artistIntroParagraph}</pre>
+
+    <h2>{copy.teaserHeading}</h2>
+    <pre>{copy.teaserParagraph}</pre>
+
+    <h2>{copy.connectionHeading}</h2>
+    <pre>{copy.connectionParagraph}</pre>
+  </>;
 }
 
 export default App;
