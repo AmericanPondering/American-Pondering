0cb47318f89e3a0efdf9d31f7585c20596a28b9e
AmericanPondering <admin@americanpondering.com>
2025-06-05 20:10:36 -0400

    Integrate backend
    Co-authored-by: Josiah Tomich <jo.launchpad@gmail.com>

--- src/components/ListForm.jsx
+++ src/components/ListForm.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -16,14 +17,17 @@ export default function ListForm() {
 }
 
 function Form() {
+    const [loading, setLoading] = useState(false);
+    const [responseJSON, setResponseJSON] = useState();
+
     const validationSchema = z.object({
         fullName: z.string().min(1, copy.nameRequiredError),
+        email: z.string().min(1, copy.emailRequiredError).email(),
         phoneNumber: z.string().refine(
             (val) => val.length === 0 || /^(\+1\s?)?(\(?\d{3}\)?[\s.-]?)\d{3}[\s.-]?\d{4}$/.test(val),
             copy.invalidPhoneNumberError
         ),
-        questions: z.string().max(500),
-        email: z.string().min(1, copy.emailRequiredError).email()
+        questions: z.string().max(500)
     });
 
     const {
@@ -34,24 +38,57 @@ function Form() {
         resolver: zodResolver(validationSchema)
     });
 
-    const onSubmit = (data) => {
-        // TODO: Implement call to backend
-        console.log(data);
+    const onSubmit = async (data) => {
+        try {
+            setLoading(true);
+            const response = await fetch("https://backend-2-996982189694.us-east5.run.app/publish", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (!response.ok) throw new Error(`Response status: ${response.status}`);
+
+            const json = await response.json();
+            setLoading(false);
+            setResponseJSON(json);
+
+        } catch (err) {
+            console.log("Error sending form data:");
+            console.log(err);
+            setLoading(false);
+        }
     }
 
     return <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("fullName")} placeholder="Name" />
+        <input {...register("fullName")} placeholder="Name (required)" />
         {errors.fullName && <span>{errors.fullName.message}</span>}
 
-        <input {...register("phoneNumber")} placeholder="Number" />
+        <input {...register("email")} placeholder="Email (required)" />
+        {errors.email && <span>{errors.email.message}</span>}
+
+        <input {...register("phoneNumber")} placeholder="Phone number" />
         {errors.phoneNumber && <span>{errors.phoneNumber.message}</span>}
 
         <input {...register("questions")} placeholder="Questions" />
         {errors.questions && <span>{errors.questions.message}</span>}
-
-        <input {...register("email")} placeholder="We'll email you back" />
-        {errors.email && <span>{errors.email.message}</span>}
         
-        <button type="submit">Submit</button>
+        <button type="submit" disabled={loading || responseJSON?.result}>Submit</button>
+
+        {/* Response UI */}
+        {responseJSON && (responseJSON?.errCode ? <InterpretErrCode errCode={responseJSON.errCode} /> : <span>Information submitted</span>)}
     </form>;
-};
+}
+
+function InterpretErrCode({ errCode }) {
+    switch (errCode) {
+        case 1:
+            return <span>Something was wrong with your information.</span>;
+        case 2:
+            return <span>Something went wrong validating your information.</span>;
+        case 3:
+            return <span>Something went wrong adding your information to the list.</span>;
+    }
+}
